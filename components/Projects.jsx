77ce322c71df7e4d05bda29ed995c5f32822bf9b
chirayu-xx <cboss1436@gmail.com>
2022-10-17 20:08:25 +0530

    revalidate

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -12,11 +12,13 @@ export default function Projects({projects}) {
     transition={{duration:1.5}}
     className='overflow-x-hidden h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500'>Projects</h3>
-
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-[#F7AB0A]'>
+            
             {/* projects  */}
             {projects.map((project,i) => (
                 <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen '>
+                    <h5 className='uppercase tracking-[20px] text-gray-500'>Click to view the Project</h5>
+                    <a href={project.linkToBuild} target="_blank">
                     <motion.img
                     initial={{
                         opacity:0,
@@ -29,6 +31,7 @@ export default function Projects({projects}) {
                     src={urlFor(project.image).url()}
                     alt=''
                     />
+                    </a>
                     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                         <h4 className='text-4xl font-semibold text-center'>
                             <span className='underline decoration-[#F7AB0A]/50'>{i+1} </span> {project.title}
