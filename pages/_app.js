f460ae42f0ed0b5c8b0d3d9e046096902b16f471
chirayu-xx <cboss1436@gmail.com>
2022-11-29 19:53:17 +0530

    SEO

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import SEO from '@bradgarropy/next-seo'
 function MyApp({ Component, pageProps }) {
   return <>
-  <SEO title="Chirayu-Portfolio" description="My portfolio" keywords={['chirayu shah', 'chirayu.tech', 'portfolio website']}
+  <SEO title="Chirayu-Portfolio" description="My portfolio" keywords={['chirayu shah', 'chirayu.tech', 'chirayu chandigarh university', 'chirayu']}
   icon='/favicon.ico'
   />
   <Component {...pageProps} />
