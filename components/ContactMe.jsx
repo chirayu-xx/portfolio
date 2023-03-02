9f3b38d6146ed750e0474fa9a78022073902f17e
chirayu-xx <cboss1436@gmail.com>
2023-03-02 19:12:50 +0530

    bugs

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -5,21 +5,20 @@ import { useRef } from "react";
 import emailjs from 'emailjs-com'
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
-import Link from "next/link";
-
 
 export default function ContactMe({}) {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const form = useRef();
   const [done, setDone] = useState(false)  
+
   const sendEmail = (e) => {
     e.preventDefault();
     const name = form.current.querySelector('#user_name').value;
     const email = form.current.querySelector('#user_email').value;
     const subject = form.current.querySelector('#subject').value;
     const message = form.current.querySelector('#message').value;
-    const loadingToast = toast.loading('Sending email...');
     try{
+      const loadingToast = toast.loading('Sending email...');
 
       if(!name || !email || !subject || !message){
         toast.error("Fill all the fields")
@@ -31,6 +30,7 @@ export default function ContactMe({}) {
     // const loadingToast = toast.loading('Sending email...');
     emailjs.sendForm('service_5y1rroh', 'template_wb2o67x', form.current, 'Rz6bzaygqNZJLIE1l')
       .then((result) => {
+        toast.dismiss(loadingToast);
         toast.success(`Thanks for contacting ${name}!`, {
           style: {
             border: '1px solid #F7AB0A',
@@ -53,7 +53,7 @@ export default function ContactMe({}) {
       toast.error(`Error sending email: ${error.message}`);
     }
     finally{
-      toast.dismiss(loadingToast);
+     
     }
   };
   return (
