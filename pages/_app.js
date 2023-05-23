c82dc1a71857bf786e55e2cd6da4605dc7fc442f
chirayu-xx <cboss1436@gmail.com>
2023-05-23 18:12:56 +0530

    ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import '../styles/globals.css'
 import SEO from '@bradgarropy/next-seo'
 import {Analytics} from '@vercel/analytics/react'
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }) {
   return <>
+  <Script
+  id='Adsense-id'
+  data-ad-client = 'ca-pub-1701950568487295'
+  async="true"
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+  onError={ (e) => { console.error('Script failed to load', e) }}
+  crossorigin="anonymous"
+  />
   <SEO title="Chirayu-Portfolio" description="Chirayu Shah:Web Developer" keywords={['chirayu shah', 'chirayu.tech','shah Chirayu','chirayu tech','chirayu chandigarh university', 'chirayu shah web developer']}
   icon='/favicon.ico'
   />
